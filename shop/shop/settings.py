b482072c739e21b14ba4fd83f9cf8e0fe86ab8a1
rasplyvajecca <rasplyvajeccaa@gmail.com>
2023-06-06 16:58:08 +0300

    add

--- shop/shop/settings.py
+++ shop/shop/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -113,8 +114,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
-    "/var/www/static/",
+os.path.join(BASE_DIR, 'static')
 ]
 
 # Default primary key field type
